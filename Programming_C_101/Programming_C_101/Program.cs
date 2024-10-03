5584432f180a2d7004fbb40ce9d83ab10a0c5ff1
Nguyen Tym <Nguyenmixx2001@gmail.com>
2024-10-03 21:34:19 +0700

    update

---
+++ Programming_C_101/Programming_C_101/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Part 1: Print a string to the console
+        Console.WriteLine("Hello World!");
+
+        // Part 2: Get user input and print it to the console
+        Console.WriteLine("Please enter your name:");
+        string userInput = Console.ReadLine();
+        Console.WriteLine("You entered: " + userInput);
+
+        // Part 3: Get two inputs from the user and print them on separate lines
+        Console.WriteLine("Please enter the first value:");
+        string firstInput = Console.ReadLine();
+        
+        Console.WriteLine("Please enter the second value:");
+        string secondInput = Console.ReadLine();
+        
+        // Print the two inputs on separate lines
+        Console.WriteLine("First value: " + firstInput + "\nSecond value: " + secondInput);
+    }
+}
